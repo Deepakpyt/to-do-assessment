f7e90386b72e0928ed11ae25200371841ca9935b
Deepakpyt <erdeepak.016@gmail.com>
2024-01-04 16:12:03 +0530

    filter-bug-resolve

--- src/Components/ListTasks.jsx
+++ src/Components/ListTasks.jsx
@@ -4,32 +4,34 @@ import { TaskContext } from '../App';
 
 export const ListTasks = () => {
 
-  const {tasks} = useContext(TaskContext);
+  const { tasks } = useContext(TaskContext);
 
-    const [todos, setTodos] = useState([]);
-    const [inProgress, setInprogrss] = useState([]);
-    const [closed, setClosed] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [inProgress, setInprogrss] = useState([]);
+  const [closed, setClosed] = useState([]);
 
-    useEffect(() => {
-        const ftodos = tasks.filter(task => task.status === 'todo');
-        const finProgress = tasks.filter(task => task.status === 'in progress');
-        const fclosed = tasks.filter(task => task.status === 'closed');
+  useEffect(() => {
+    if (tasks !== null) {
+      const ftodos = tasks.filter(task => task.status === 'todo');
+      const finProgress = tasks.filter(task => task.status === 'in progress');
+      const fclosed = tasks.filter(task => task.status === 'closed');
 
-        setTodos(ftodos);
-        setInprogrss(finProgress);
-        setClosed(fclosed);
+      setTodos(ftodos);
+      setInprogrss(finProgress);
+      setClosed(fclosed);
+    }
 
-    },[tasks]);
+  }, [tasks]);
 
-    const status = ["todo", "in progress", "closed"];
+  const status = ["todo", "in progress", "closed"];
 
 
   return (
     <div className='flex flex-wrap gap-16'>
-        {/* <h2>List Items</h2> */}
-        {status.map((status, index) => {
-            return <Section key={index} status={status} todos={todos} inProgress={inProgress} closed={closed} />
-        })}
+      {/* <h2>List Items</h2> */}
+      {status.map((status, index) => {
+        return <Section key={index} status={status} todos={todos} inProgress={inProgress} closed={closed} />
+      })}
     </div>
   )
 }
